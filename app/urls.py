a5893d2fcf2226bf928fd72689b75d986f3c2429
Bharath Gopalswamy <110247438+bharathgopalswamy@users.noreply.github.com>
2023-03-03 20:38:16 +0530

    Add files via upload

---
+++ app/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register_doc, name="register"),
+    path('registerpat/', views.register_pat, name='registerpat'),
+    path('login/', views.login, name='login'),
+    path('blogs/', views.blogsall, name='blogs'),
+    path('drafts/', views.drafts, name='drafts'),
+    path('myblog/', views.myblogs, name='myblogs'),
+    path('create/', views.createBlog, name='create'),
+    path('mhealth/', views.mhealth, name='mhealth'),
+    path('heartdis/', views.heartdis, name='heartdis'),
+    path('covid19/', views.covid19, name='covid19'),
+    path('immun/', views.immun, name='immun'),
+    path('appoint/', views.appointment, name='appoint'),
+    path('formapp/<int:pk>', views.create_appoint, name='appcreate'),
+    path('myapps/', views.view_allapp, name='myapps'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout')
+]
